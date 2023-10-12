d78a2dee4d96b3161350b54b2247127bec95794c
Kleo Zane <kleo.zane@hotmail.com>
2023-10-12 17:36:44 +0200

    mbaruam kurrikulen

--- Controllers/CurriculumController.cs
+++ Controllers/CurriculumController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using School.Data;
+using School.Models;
 
 namespace School.Controllers
 {
@@ -36,6 +37,71 @@ namespace School.Controllers
 
             var curriculum = await _context.Curriculums.Include(x => x.Subjects).FirstOrDefaultAsync(m => m.Id == id);
             return View(curriculum);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Weeks,Hours")] Curriculum curriculum)
+        {
+            _context.Add(curriculum);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Curriculums == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var curriculum = await _context.Curriculums.FindAsync(id);
+            return View(curriculum);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Weeks,Hours")] Curriculum curriculum)
+        {
+            if (id != curriculum.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Update(curriculum);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_context.Curriculums == null)
+            {
+                return Problem("Nuk ka kurrikula!");
+            }
+            var curriculum = await _context.Curriculums.FindAsync(id);
+            if (curriculum != null)
+            {
+                _context.Curriculums.Remove(curriculum);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
